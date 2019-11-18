1fdc66af34bb321e8cb19c156db4fd217df74e06
Felix Dangel <felix.dangel@tuebingen.mpg.de>
2019-11-18 18:41:41 +0100

    Add poorly formatted example function

---
+++ snippets/poor_format.py
@@ -0,0 +1,4 @@
+def sum_five_arguments(argument1, argument2, argument3 , keyword_argument1=123, keyword_argument2 = 5):
+    """Returns a sum of all arguments."""
+    sum_of_args=argument1+argument2+argument3+keyword_argument1 + keyword_argument2
+    return sum_of_args
